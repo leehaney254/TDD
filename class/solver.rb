85f1b74e83616d778783e6cafde565fc2ba9b323
Christian Ani <iamchristianani@gmail.com>
2023-03-16 16:20:44 +0100

    Add condition for numbers divisible by 3 and 5

--- class/solver.rb
+++ class/solver.rb
@@ -16,11 +16,12 @@ class Solver
   end
 
   def fizzbuzz(number)
-    if number % 3 == 0
+    if number % 3 == 0 && number % 5 == 0
+      'fizzbuzz'
+    elsif number % 3 == 0
       'fizz'
     elsif number % 5 == 0
       'buzz'
     end
   end
 end
-Test to return buzz for numbers not divisible by 3
