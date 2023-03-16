1a88fbaf2525e7f22324a2fa19444a2f92104397
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-16 13:38:12 +0300

    Feat: Add condition for negative number

--- class/solver.rb
+++ class/solver.rb
@@ -3,6 +3,11 @@ class Solver
   def factorial(number)
     result = 1
     return result if number == 0 || number == 1
+
+    if number < 0
+      raise StandardError, 'Number cannot be negative'
+    end
+
     for i in (number).downto(1) do
       result *= i
     end
