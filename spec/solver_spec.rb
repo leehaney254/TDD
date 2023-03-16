5d6d61e87ce4148c86e414e2c03ca580b3e2637b
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-16 12:53:24 +0300

    Feat: Add test for factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe Solver do
 
+  before :each do
+    @solver = Solver.new
+  end
+
+  context 'When working with solver' do
+    expect (@solver.factorial(5)).to eq 20
+  end
+
+
 end
